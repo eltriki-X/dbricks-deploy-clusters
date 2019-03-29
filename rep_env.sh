debad8317142b96198c1303325e2900cda5fbf2c
eltriki-X <eltriki@outlook.com>
2019-03-29 12:03:21 +0100

    Update rep_env.sh

--- rep_env.sh
+++ rep_env.sh
@@ -3,8 +3,8 @@ cd /userdbks
 sed -i 's|dbrick_wkspace|'"$dbrick_wkspace"'|g' .databrickscfg
 sed -i 's|dbrick_tokenpw|'"$dbrick_tokenpw"'|g' .databrickscfg
 #REplace in external-metastore connection
-sed -i 's|sql_srv|'"$sql_srv"'|g' external-metastore.sh
-sed -i 's|sql_dbs|'"$sql_dbs"'|g' external-metastore.sh
-sed -i 's|sqluser|'"$sqluser"'|g' external-metastore.sh
-sed -i 's|sqlpass|'"$sqlpass"'|g' external-metastore.sh
+sed -i 's|sql_srv|'"$sql_srv"'|g' external_metastore.sh
+sed -i 's|sql_dbs|'"$sql_dbs"'|g' external_metastore.sh
+sed -i 's|sqluser|'"$sqluser"'|g' external_metastore.sh
+sed -i 's|sqlpass|'"$sqlpass"'|g' external_metastore.sh
 exec "$@"
